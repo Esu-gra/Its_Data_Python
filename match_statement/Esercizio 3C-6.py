ad74c039c5820197cdee832278eadfde031e150a
Esu-gra <esugrappa@gmail.com>
2025-02-25 11:38:50 +0100

    commit

---
+++ match_statement/Esercizio 3C-6.py
@@ -0,0 +1,37 @@
+
+mammiferi=["cane","gatto","cavallo","elefante","leone"]
+rettili=["serpente","lucertola","tartaruga","coccodrillo"]
+uccelli=["acquila","pappagallo","gufo","falco"]
+pesci=[" squalo","trota","salmone","carpa"]
+
+habitat_t=["acqua","aria","terra"]
+
+     
+
+
+animal=(input("inserire animale: ")).lower()
+habitat=input("inserire habitat: acqua,aria,terra: ").strip().lower()
+
+
+
+match animal:
+        case _ if animal in mammiferi and habitat==habitat_t[2]:
+            print("Categoria: Mammiferi")
+            print(f"{animal} è uno degli animali che vive sulla terra")
+    
+        case _ if animal in rettili :
+            print("Categoria: Rettili")
+        case _ if animal=="coccodrillo":
+            print(f"Il {animal} vive sia in terra che in acqua")
+
+
+        case _ if animal in uccelli and  habitat==habitat_t[1]:
+            print("Categoria: Uccelli")
+            print(f"{animal} il suo ahabitat è l'aria")
+
+            
+        case _ if animal in pesci and habitat==habitat_t[0]:
+            print("Categoria: Pesci")
+            print(f"{animal} il suo habitat è l'acqua")
+        case _:
+            print("Categoria non conosciuta")
