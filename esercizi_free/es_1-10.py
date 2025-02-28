b83b5f67f20a25a4eb2849fa11271a2e49d8f975
Esu-gra <esugrappa@gmail.com>
2025-02-28 13:57:41 +0100

    boh

---
+++ esercizi_free/es_1-10.py
@@ -0,0 +1,37 @@
+#ese-1
+i=0
+
+while True:
+    if i < 10:
+        i=i+1
+        # print(i)
+        
+    else:
+
+        break
+
+
+
+
+#°°°°°
+while i <= 9:
+    
+    i+=1
+
+
+
+#####################
+
+# row=5
+
+# for i in range(1,row+1,1):
+#     for j in range(1,i,+1):
+#         print(j,end=' ')
+#     print("")
+
+###########################
+
+# for i in range(2,22,2):
+# #    print(i)
+
+#####################
