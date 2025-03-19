094be785660714dc485cf5b402d42ac83cc99c26
Esu-gra <esugrappa@gmail.com>
2025-03-19 15:28:35 +0100

    import

--- function_/import/test_saluto.py
+++ function_/import/test_saluto.py
@@ -3,10 +3,35 @@
 # import_saluto.greet("Anna")
 
 
+
+
+
+
+
+
 # secondo modo : con l'alians ---> dandogli un nuovo nome
 
 
+# import import_saluto as saluto
 
-import import_saluto as saluto
+# saluto.greet("Esu")
 
-saluto.greet("Esu")
+
+
+
+#se voglio importare sola la funzione 
+
+
+# from import_saluto import greet
+
+# greet("Aldo")
+
+
+
+
+
+################
+
+from import_saluto import greet as g
+
+g("Polo")
