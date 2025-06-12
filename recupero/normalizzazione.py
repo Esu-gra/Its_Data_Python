2484d4e4fd36af667a6594524e425d691d3df0c8
Esu-gra <esugrappa@gmail.com>
2025-06-12 17:30:03 +0200

    .

---
+++ recupero/normalizzazione.py
@@ -0,0 +1,8 @@
+'''
+Frequenza di Parole Uniche con Normalizzazione
+Scrivi una funzione che prende una stringa di testo (contenente eventualmente
+punteggiatura, lettere maiuscole e minuscole, spazi bianchi) e restituisce un dizionario che
+associa a ciascuna parola unica (in minuscolo, privata della punteggiatura iniziale/finale) il
+numero di occorrenze.
+'''
+
