3caa9dfca67983fd6b5be69b53f2def9cdbfd0ba
Esu-gra <esugrappa@gmail.com>
2025-10-07 11:34:05 +0200

    flask

---
+++ Lezione_23/Flask/flask_prova.py
@@ -0,0 +1,15 @@
+from flask import  Flask
+
+
+
+app=Flask(__name__)
+@app.route('/')
+def home()->str:
+    return "<h3>Hello,World!</h3>"
+# app.run(debug=True,host='127.0.0.1',port=5000)  
+
+@app.route('/data')
+def data()->str:
+    return "<h3>sei uno studente Datanlyst!</h3>"
+
+
